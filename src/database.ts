56e132de012b2070f5170c426613748bd2b8db4a
krtabares <krtabares@gmail.com>
2024-05-19 23:06:48 -0400

    delete logs

--- src/database.ts
+++ src/database.ts
@@ -3,12 +3,12 @@ import config from "./config/config";
 import SubscripcionesPush from './models/subscripcionesPush.model';
 import ColaNotificaciones from './models/colaNotificaciones.model';
 
-console.log(config);
 
 export const connectDB = async () => {
    try {
 
       const sequelize = new Sequelize({
+         logging:false,
          host:config.DB.HOST,
          database: config.DB.DBNAME,
          dialect: 'mysql',
